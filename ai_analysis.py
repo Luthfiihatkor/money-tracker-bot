72b7196a62e8d81c511e632a711730c1b97180f6
Luthfiihatkor <134399103+Luthfiihatkor@users.noreply.github.com>
2026-04-06 01:38:26 +0700

    Update ai_analysis.py

--- ai_analysis.py
+++ ai_analysis.py
@@ -1,1 +1,16 @@
+from database import c
 
+def analyze(user):
+
+    c.execute("SELECT kategori,SUM(jumlah) FROM transaksi WHERE user=? AND tipe='out' GROUP BY kategori",(user,))
+    data=c.fetchall()
+
+    text="🧠 Analisa Keuangan\n\n"
+
+    if not data:
+        return "Belum ada data."
+
+    for d in data:
+        text+=f"{d[0]} : {d[1]}\n"
+
+    return text
