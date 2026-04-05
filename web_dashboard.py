3f88d421675fd36a2db4324095d168379cbcbe1c
Luthfiihatkor <134399103+Luthfiihatkor@users.noreply.github.com>
2026-04-06 03:35:11 +0700

    Update web_dashboard.py

--- web_dashboard.py
+++ web_dashboard.py
@@ -1,46 +1,75 @@
-from flask import Flask,render_template_string
-from database import get_transactions
+from flask import Flask, render_template_string
+from services.finance_service import get_transactions
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 
 @app.route("/")
-def home():
+def dashboard():
 
-    data=get_transactions()
+    data = get_transactions()
 
-    html="""
-    <h1>Finance Dashboard</h1>
+    income = 0
+    expense = 0
+
+    for d in data:
+
+        if d[1] == "income":
+            income += d[2]
+
+        if d[1] == "expense":
+            expense += d[2]
+
+    balance = income - expense
+
+    html = """
+    <h1>Personal Finance Dashboard</h1>
+
+    <h2>Summary</h2>
+
+    <p>Income : {{income}}</p>
+    <p>Expense : {{expense}}</p>
+    <p>Balance : {{balance}}</p>
+
+    <h2>Transactions</h2>
 
     <table border=1>
 
     <tr>
-    <th>ID</th>
-    <th>Type</th>
-    <th>Amount</th>
-    <th>Category</th>
-    <th>Note</th>
-    <th>Date</th>
+        <th>ID</th>
+        <th>Type</th>
+        <th>Amount</th>
+        <th>Category</th>
+        <th>Note</th>
+        <th>Date</th>
     </tr>
 
     {% for row in data %}
-
     <tr>
-    <td>{{row[0]}}</td>
-    <td>{{row[1]}}</td>
-    <td>{{row[2]}}</td>
-    <td>{{row[3]}}</td>
-    <td>{{row[4]}}</td>
-    <td>{{row[5]}}</td>
+        <td>{{row[0]}}</td>
+        <td>{{row[1]}}</td>
+        <td>{{row[2]}}</td>
+        <td>{{row[3]}}</td>
+        <td>{{row[4]}}</td>
+        <td>{{row[5]}}</td>
     </tr>
-
     {% endfor %}
 
     </table>
     """
 
-    return render_template_string(html,data=data)
+    return render_template_string(
+        html,
+        data=data,
+        income=income,
+        expense=expense,
+        balance=balance
+    )
 
 
-if __name__=="__main__":
-    app.run()
+if __name__ == "__main__":
+
+    app.run(
+        host="0.0.0.0",
+        port=8080
+    )
