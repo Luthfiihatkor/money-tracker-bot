148e7757cb888eedb26b610873e2b2b24f5ac791
Luthfiihatkor <134399103+Luthfiihatkor@users.noreply.github.com>
2026-04-06 03:24:56 +0700

    Update export_service.py

--- services/export_service.py
+++ services/export_service.py
@@ -1,1 +1,18 @@
+import pandas as pd
+from services.finance_service import get_transactions
 
+
+def export_excel():
+
+    data = get_transactions()
+
+    df = pd.DataFrame(
+        data,
+        columns=["id","type","amount","category","note","date"]
+    )
+
+    file = "finance_report.xlsx"
+
+    df.to_excel(file, index=False)
+
+    return file
