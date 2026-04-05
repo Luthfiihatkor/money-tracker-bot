03030b7d76e8d5944db73a20607c8eb85dae5d54
Luthfiihatkor <134399103+Luthfiihatkor@users.noreply.github.com>
2026-04-06 03:23:24 +0700

    Update category_service.py

--- services/category_service.py
+++ services/category_service.py
@@ -1,1 +1,23 @@
+def detect_category(note):
 
+    n = note.lower()
+
+    if "makan" in n or "food" in n:
+        return "food"
+
+    if "kopi" in n:
+        return "food"
+
+    if "bensin" in n:
+        return "transport"
+
+    if "belanja" in n:
+        return "shopping"
+
+    if "internet" in n:
+        return "utility"
+
+    if "bisnis" in n:
+        return "business"
+
+    return "other"
