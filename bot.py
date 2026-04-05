643787aff416b94b38848da62e7de0671f629408
Luthfiihatkor <134399103+Luthfiihatkor@users.noreply.github.com>
2026-04-06 01:32:24 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,37 +1,78 @@
-
 import telebot
+import re
 import os
+from datetime import datetime
+from database import conn,c
 
 TOKEN = os.getenv("TOKEN")
+
 bot = telebot.TeleBot(TOKEN)
 
-balance = 0
+
+def saldo(user):
+
+    c.execute("SELECT SUM(jumlah) FROM transaksi WHERE user=? AND tipe='in'",(user,))
+    masuk=c.fetchone()[0] or 0
+
+    c.execute("SELECT SUM(jumlah) FROM transaksi WHERE user=? AND tipe='out'",(user,))
+    keluar=c.fetchone()[0] or 0
+
+    return masuk-keluar
+
+
 
 @bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message,
-    "💰 Money Tracker Bot\n\n"
-    "Perintah:\n"
-    "/add 10000 = tambah pemasukan\n"
-    "/out 5000 = catat pengeluaran\n"
-    "/saldo = cek saldo")
+def start(m):
 
-@bot.message_handler(commands=['add'])
-def add_money(message):
-    global balance
-    amount = int(message.text.split()[1])
-    balance += amount
-    bot.reply_to(message, f"✅ Pemasukan ditambah\nSaldo: {balance}")
+    bot.reply_to(m,"""
+👑 PERSONAL FINANCE OS
 
-@bot.message_handler(commands=['out'])
-def out_money(message):
-    global balance
-    amount = int(message.text.split()[1])
-    balance -= amount
-    bot.reply_to(message, f"💸 Pengeluaran dicatat\nSaldo: {balance}")
+Contoh:
+
+beli kopi 15000
+makan 20000
+jual belut 500000
+
+Commands:
+
+/saldo
+/stat
+/history
+/analysis
+""")
+
 
 @bot.message_handler(commands=['saldo'])
-def check_balance(message):
-    bot.reply_to(message, f"💰 Saldo kamu: {balance}")
+def saldo_cmd(m):
+
+    bot.reply_to(m,f"💰 Saldo : {saldo(m.from_user.id)}")
+
+
+
+@bot.message_handler(func=lambda m: True)
+def catat(m):
+
+    text=m.text.lower()
+
+    angka=re.findall(r'\d+',text)
+
+    if not angka:
+        return
+
+    jumlah=int(angka[0])
+
+    if "jual" in text:
+        tipe="in"
+    else:
+        tipe="out"
+
+    c.execute("""
+    INSERT INTO transaksi VALUES(NULL,?,?,?,?,?,?,?)
+    """,(m.from_user.id,tipe,jumlah,"general","cash",text,str(datetime.now().date())))
+
+    conn.commit()
+
+    bot.reply_to(m,"✅ transaksi dicatat")
+
 
 bot.infinity_polling()
