0a826f1693a9a7952ac3047970894e618e85f4a0
Luthfiihatkor <134399103+Luthfiihatkor@users.noreply.github.com>
2026-04-06 03:23:52 +0700

    Update finance_service.py

--- services/finance_service.py
+++ services/finance_service.py
@@ -1,1 +1,33 @@
+from database import connect
+from datetime import datetime
 
+
+def add_transaction(t_type, amount, category, note):
+
+    conn = connect()
+    cur = conn.cursor()
+
+    cur.execute(
+        """
+        INSERT INTO transactions(type,amount,category,note,date)
+        VALUES(%s,%s,%s,%s,%s)
+        """,
+        (t_type, amount, category, note, datetime.now())
+    )
+
+    conn.commit()
+    conn.close()
+
+
+def get_transactions():
+
+    conn = connect()
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM transactions")
+
+    data = cur.fetchall()
+
+    conn.close()
+
+    return data
