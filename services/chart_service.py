cd887fd47bfb9c19b9ab02c145587ce8636317aa
Luthfiihatkor <134399103+Luthfiihatkor@users.noreply.github.com>
2026-04-06 03:24:24 +0700

    Update chart_service.py

--- services/chart_service.py
+++ services/chart_service.py
@@ -1,1 +1,27 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from services.finance_service import get_transactions
 
+
+def category_chart():
+
+    data = get_transactions()
+
+    df = pd.DataFrame(data,
+        columns=["id","type","amount","category","note","date"])
+
+    expense = df[df["type"]=="expense"]
+
+    chart = expense.groupby("category")["amount"].sum()
+
+    chart.plot(kind="pie", autopct="%1.1f%%")
+
+    plt.title("Expense Distribution")
+
+    file = "category_chart.png"
+
+    plt.savefig(file)
+
+    plt.close()
+
+    return file
