86ac79955c850eb1394f2623b2401fa4a15f565a
Luthfiihatkor <134399103+Luthfiihatkor@users.noreply.github.com>
2026-04-06 01:38:51 +0700

    Update export_excel.py

--- export_excel.py
+++ export_excel.py
@@ -1,1 +1,20 @@
+import openpyxl
+from database import c
 
+def export(user):
+
+    wb=openpyxl.Workbook()
+    ws=wb.active
+
+    ws.append(["Tanggal","Tipe","Kategori","Jumlah","Wallet","Note"])
+
+    c.execute("SELECT tanggal,tipe,kategori,jumlah,wallet,note FROM transaksi WHERE user=?",(user,))
+    data=c.fetchall()
+
+    for d in data:
+        ws.append(d)
+
+    file="report.xlsx"
+    wb.save(file)
+
+    return file
