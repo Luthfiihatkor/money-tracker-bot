00b52e5ecd2895a424f545bbc61c8f306d7bc831
Luthfiihatkor <134399103+Luthfiihatkor@users.noreply.github.com>
2026-04-06 03:22:39 +0700

    Update models.py

--- models.py
+++ models.py
@@ -1,1 +1,39 @@
+from database import connect
 
+
+def init_db():
+
+    conn = connect()
+    cur = conn.cursor()
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS transactions(
+        id SERIAL PRIMARY KEY,
+        type TEXT,
+        amount INT,
+        category TEXT,
+        note TEXT,
+        date TIMESTAMP
+    )
+    """)
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS goals(
+        id SERIAL PRIMARY KEY,
+        name TEXT,
+        target INT,
+        saved INT
+    )
+    """)
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS bills(
+        id SERIAL PRIMARY KEY,
+        name TEXT,
+        amount INT,
+        due_date DATE
+    )
+    """)
+
+    conn.commit()
+    conn.close()
