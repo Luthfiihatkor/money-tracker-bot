97e73fd6df161685272231f25e424f683fcd0a6d
Luthfiihatkor <134399103+Luthfiihatkor@users.noreply.github.com>
2026-04-06 01:45:58 +0700

    Update database.py

--- database.py
+++ database.py
@@ -1,19 +1,53 @@
 import sqlite3
 
 conn = sqlite3.connect("money.db", check_same_thread=False)
-c = conn.cursor()
+cursor = conn.cursor()
 
-c.execute("""
-CREATE TABLE IF NOT EXISTS transaksi(
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS transactions (
 id INTEGER PRIMARY KEY AUTOINCREMENT,
-user INTEGER,
-tipe TEXT,
-jumlah INTEGER,
-kategori TEXT,
-wallet TEXT,
+user_id INTEGER,
+type TEXT,
+amount INTEGER,
+category TEXT,
 note TEXT,
-tanggal TEXT
+date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )
 """)
 
 conn.commit()
+
+
+def add_transaction(user_id, tipe, amount, category, note):
+    cursor.execute(
+        "INSERT INTO transactions (user_id,type,amount,category,note) VALUES (?,?,?,?,?)",
+        (user_id, tipe, amount, category, note),
+    )
+    conn.commit()
+
+
+def get_transactions(user_id):
+    cursor.execute(
+        "SELECT type,amount,category,note,date FROM transactions WHERE user_id=?",
+        (user_id,),
+    )
+    return cursor.fetchall()
+
+
+def get_balance(user_id):
+
+    cursor.execute(
+        "SELECT SUM(amount) FROM transactions WHERE user_id=? AND type='income'",
+        (user_id,),
+    )
+
+    income = cursor.fetchone()[0] or 0
+
+    cursor.execute(
+        "SELECT SUM(amount) FROM transactions WHERE user_id=? AND type='expense'",
+        (user_id,),
+    )
+
+    expense = cursor.fetchone()[0] or 0
+
+    return income - expense
