a50c0cae533128e3154d1a95747598586ef41978
Luthfiihatkor <134399103+Luthfiihatkor@users.noreply.github.com>
2026-04-06 03:21:55 +0700

    Update database.py

--- database.py
+++ database.py
@@ -1,53 +1,5 @@
-import sqlite3
+import psycopg2
+from config import DATABASE_URL
 
-conn = sqlite3.connect("money.db", check_same_thread=False)
-cursor = conn.cursor()
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS transactions (
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-user_id INTEGER,
-type TEXT,
-amount INTEGER,
-category TEXT,
-note TEXT,
-date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
-""")
-
-conn.commit()
-
-
-def add_transaction(user_id, tipe, amount, category, note):
-    cursor.execute(
-        "INSERT INTO transactions (user_id,type,amount,category,note) VALUES (?,?,?,?,?)",
-        (user_id, tipe, amount, category, note),
-    )
-    conn.commit()
-
-
-def get_transactions(user_id):
-    cursor.execute(
-        "SELECT type,amount,category,note,date FROM transactions WHERE user_id=?",
-        (user_id,),
-    )
-    return cursor.fetchall()
-
-
-def get_balance(user_id):
-
-    cursor.execute(
-        "SELECT SUM(amount) FROM transactions WHERE user_id=? AND type='income'",
-        (user_id,),
-    )
-
-    income = cursor.fetchone()[0] or 0
-
-    cursor.execute(
-        "SELECT SUM(amount) FROM transactions WHERE user_id=? AND type='expense'",
-        (user_id,),
-    )
-
-    expense = cursor.fetchone()[0] or 0
-
-    return income - expense
+def connect():
+    return psycopg2.connect(DATABASE_URL)
